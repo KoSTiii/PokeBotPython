fe061f0e94ef63442a02bb91e80a60203d3c4864
Luka Koštomaj <luka@bogve.org>
2016-07-25 02:20:38 +0200

    pokebot import resolved

--- PokeBot/pokebot.py
+++ PokeBot/pokebot.py
@@ -1,9 +1,8 @@
 import logging
 
-from PokeApi import config
 from PokeApi.pokeapi import PokeApi
 from PokeApi.auth import GoogleLogin, PTCLogin
-from PokeBot.config import BotConfig
+from PokeBot.config import BotConfig, SUPPORTED_PROVIDERS
 
 
 """ Main class of pokebot 
@@ -12,12 +11,12 @@ class PokeBot(object):
 
     """ Initialize the Master PokeBot
     """
-    def __init__(self, config_):
-        if not isinstance(config_, BotConfig):
+    def __init__(self, config):
+        if not isinstance(config, BotConfig):
             raise TypeError('arg config is not type of class Config')
         
         self.logger = logging.getLogger(__name__)
-        self.config = config_
+        self.config = config
         
         self.auth = self._authorize()
         self.pokeapi = PokeApi(self.auth, self.config.get_location_manager())
@@ -32,7 +31,7 @@ class PokeBot(object):
         else:
             self.logger.error('wrong provider specified')
             raise ValueError('{0}: provider is not supported. Supported providers: {1}'
-                            .format(self.config.provider, config.SUPPORTED_PROVIDERS))
+                             .format(self.config.provider, SUPPORTED_PROVIDERS))
 
     """ Update bot
     """
