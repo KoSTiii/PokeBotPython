4ef3adf28eb23dd9b48d5673dff5727c66866042
Luka Koštomaj <luka@bogve.org>
2016-07-27 07:03:05 +0200

    ready for time manipulation

--- PokeBot/helpers.py
+++ PokeBot/helpers.py
@@ -1,4 +1,13 @@
+import time
 
 
 def clamp(num, smallest, largest):
     return max(smallest, min(num, largest))
+
+
+def expired_time(time_in_s):
+    """
+    Calculate if time is expired
+    """
+    #now = time.time()
+    #if time_in_s - now > 0:
